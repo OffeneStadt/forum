aea370d4e569a20682a1b9a573b5127833d128e4
umut0 <umut0815@yahoo.com>
2017-02-06 20:14:08 +0100

    apps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var apps = document.getElementsByClassName('app');
 
+    for(var i=0; i< apps.length;i++){
+        apps[i].addEventListener('click', function(){
+            window.open(this.getAttribute('data-href'), '_blank');
+
+        })
+    }
+});
